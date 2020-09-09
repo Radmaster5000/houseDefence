744c5b457aa6f0d2249b21a6cc65aa68956a880e
Radmaster5000 <phil.radford5000@gmail.com>
2020-09-09 10:28:38 +0100

    moved the attack mechanic into a 'round()' function

--- classesAndFunctions.py
+++ classesAndFunctions.py
@@ -21,6 +21,77 @@ class enemy:
 def die_roll(n):
 	return random.randint(0, n)
 
+def printy(text, textFile):
+	print(text)
+	print(text, file=textFile)
+
+
+
+
+def round(roundNum, playerObj, textFile):
+	level = roundNum
+	player = playerObj
+	f = textFile
+	enemyArray = spawn_zombies(level)
+
+	printy('#############################',f)
+	printy('ENEMY TURN',f)
+
+
+	for enemy in enemyArray:
+		roll = die_roll(6)
+		printy("The enemy roles an attack: " + str(roll),f)
+	
+		if (roll > player.defence):
+			printy("the zombie successfully attacks!",f)
+			player.health -= enemy.attack
+		else:
+			printy("you fight the zombie off",f)
+			player.defence -= enemy.attack
+	
+	printy("player's health = " + str(player.health),f)
+	printy("player's defence is = " + str(player.defence),f)
+	for enemy in enemyArray:
+		printy(str(enemy.uid) + "'s health = " + str(enemy.health),f)
+
+	printy("##############################",f)
+	printy('PLAYER TURN',f)
+
+	# Initialise target outside of attack loop
+	# This identifies the target in the enemyArray
+	target = 0
+
+	for turns in range(0, player.speed):
+	
+		if (len(enemyArray) == 0):
+			print("Congrats! You won!")
+			quit()
+
+		# It the target variable reaches the end of the array, reset it
+		if (target >= len(enemyArray)):
+			target = 0
+
+		roll = die_roll(6)
+		printy("The player rolled a: " + str(roll),f)
+	
+		if (roll > enemyArray[target].defence):
+			printy("you successfully attack " + enemyArray[target].uid,f)
+			enemyArray[target].health -= player.attack
+			if (enemyArray[target].health <= 0):
+				printy("You killed " + enemyArray[target].uid,f)
+				enemyArray.pop(target)
+				target -= 1
+		else:
+			printy("you miss the zombie",f)
+			enemyArray[target].defence -= player.attack
+
+		# Increment variable to target the next enemy in the array
+		target += 1
+	for enemy in enemyArray:	
+		printy(enemy.uid + "'s health = " + str(enemy.health),f)
+		printy(enemy.uid + "'s defence is = " + str(enemy.defence),f)
+
+	return player
 
 def spawn_zombies(number_of_enemies):
 	array_of_zombies = []
@@ -29,4 +100,3 @@ def spawn_zombies(number_of_enemies):
 		zombie = enemy(3,1,3, uid)
 		array_of_zombies.append(zombie)
 	return array_of_zombies
-	
--- humanZombie.py
+++ humanZombie.py
@@ -7,72 +7,14 @@ from classesAndFunctions import *
 f = open("gameLog.txt", "w")
 
 # custom function to print to the console and to the game log file at the same time
-def printy(text):
-	print(text)
-	print(text, file=f)
-
-player = human(3,3,3,6)
-
-level = 3
-
-enemyArray = spawn_zombies(level)
-
-printy('#############################')
-printy('ENEMY TURN')
 
 
-for enemy in enemyArray:
-	roll = die_roll(6)
-	printy("The enemy roles an attack: " + str(roll))
-	
-	if (roll > player.defence):
-		printy("the zombie successfully attacks!")
-		player.health -= enemy.attack
-	else:
-		printy("you fight the zombie off")
-		player.defence -= enemy.attack
-	
-printy("player's health = " + str(player.health))
-printy("player's defence is = " + str(player.defence))
-for enemy in enemyArray:
-	printy(str(enemy.uid) + "'s health = " + str(enemy.health))
+player = human(3,3,3,3)
 
-printy("##############################")
-printy('PLAYER TURN')
+round(3, player, f)
 
-# Initialise target outside of attack loop
-# This identifies the target in the enemyArray
-target = 0
-
-for turns in range(0, player.speed):
-	
-	if (len(enemyArray) == 0):
-		print("Congrats! You won!")
-		quit()
-
-	# It the target variable reaches the end of the array, reset it
-	if (target >= len(enemyArray)):
-		target = 0
-
-	roll = die_roll(6)
-	printy("The player rolled a: " + str(roll))
-	
-	if (roll > enemyArray[target].defence):
-		printy("you successfully attack " + enemyArray[target].uid)
-		enemyArray[target].health -= player.attack
-		if (enemyArray[target].health <= 0):
-			printy("You killed " + enemyArray[target].uid)
-			enemyArray.pop(target)
-			target -= 1
-	else:
-		printy("you miss the zombie")
-		enemyArray[target].defence -= player.attack
-
-	# Increment variable to target the next enemy in the array
-	target += 1
-for enemy in enemyArray:	
-	printy(enemy.uid + "'s health = " + str(enemy.health))
-	printy(enemy.uid + "'s defence is = " + str(enemy.defence))
+printy("player's health = " + str(player.health),f)
+printy("player's defence is = " + str(player.defence),f)
 
 # close the game log text file
 f.close()
