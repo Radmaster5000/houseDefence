148bf1db67a1f3d542165e33bd701ebc5bd6d589
Radmaster5000 <phil.radford5000@gmail.com>
2020-09-28 13:51:23 +0100

    Think I fixed the problem trying to link the parsed words to the appropriate object attributes. That sounds more complicated than it needs to.

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Added a custom print function that will print to the console and also save the o
 
 ************************
 
+UPDATE 28/09/20
+
+I think I've got a satisfactory regex parser working, but I don't know if it's much better than the branched if statements I had before. I'll have to catch any mis-spellings and try to incorporate it into the main program. It requires more lines, but will also catch any mixed-case letterings.
+
+*********
+
 UPDATE: 23/09/20
 
 Currently playing with regex and parsing the player's inputs when upgrading. Probably not going to look like I'm doing much for a while as I try to get my head round this.
--- regex.py
+++ regex.py
@@ -1,5 +1,22 @@
 import re
 
+class building:
+	def __init__(self, health, attack, defence):
+		self.health = health
+		self.attack = attack
+		self.defence = defence
+
+class human:
+	
+	def __init__(self, health, attack, defence, speed):
+		self.health = health
+		self.attack = attack
+		self.defence = defence
+		self.speed = speed
+
+player = human('human health','human attack','human defence','human speed')
+safehouse = building('safehouse health','safehouse attack','safehouse defence')
+
 string1 = 'player'
 string2 = 'player speed'
 string3 = 'playerspeed'
@@ -32,54 +49,67 @@ twoWordInput = re.compile('(\w+)\s(\w+)', re.IGNORECASE)
 
 # if the player's input is saved as choice
 # example... choice = 'player strength'
-choice = string4
+choice = 'safehouse speed'
 
-parsed = twoWordInput.match('PLAYER ATTACK')
+parsed = twoWordInput.match(choice)
 
 
-parsed.group() # 'player strength'
-parsed.group(1) # 'player'
-parsed.group(2) # 'strength'
+#parsed.group() # 'player strength'
+#parsed.group(1) # 'player'
+#parsed.group(2) # 'strength'
+
+##if (pl.match(parsed.group(1))):
+##	if (he.match(parsed.group(2))):
+##		print('increased player health')
+##		print('decrement points')
+##		print('print player stats')
+##	elif (at.match(parsed.group(2))):
+##		print('increased player attack')
+##		print('decrement points')
+##		print('print player stats')
+##	elif (de.match(parsed.group(2))):
+##		print('increased player defence')
+##		print('decrement points')
+##		print('print player stats')
+##	elif (sp.match(parsed.group(2))):
+##		print('increased player speed')
+##		print('decrement points')
+##		print('print player stats')
+##	else:
+##		print('re-loop player')
+##		# print error message and re-loop
+##elif (sa.match(parsed.group(1))):
+##	if (he.match(parsed.group(2))):
+##		print('increased safehouse health')
+##		print('decrement points')
+##		print('print safehouse stats')
+##	elif (at.match(parsed.group(2))):
+##		print('increased safehouse attack')
+##		print('decrement points')
+##		print('print safehouse stats')
+##	elif (de.match(parsed.group(2))):
+##		print('increased safehouse defence')
+##		print('decrement points')
+##		print('print safehouse stats')
+##	else:
+##		print('re-loop safehouse')
+##		# print error message and re-loop
+##else:
+##	print('re-loop epic fail')
+##	# print error message and re-loop
 
 if (pl.match(parsed.group(1))):
-	if (he.match(parsed.group(2))):
-		print('increased player health')
-		print('decrement points')
-		print('print player stats')
-	elif (at.match(parsed.group(2))):
-		print('increased player attack')
-		print('decrement points')
-		print('print player stats')
-	elif (de.match(parsed.group(2))):
-		print('increased player defence')
-		print('decrement points')
-		print('print player stats')
-	elif (sp.match(parsed.group(2))):
-		print('increased player speed')
-		print('decrement points')
-		print('print player stats')
-	else:
-		print('re-loop player')
-		# print error message and re-loop
+	objVar = player
 elif (sa.match(parsed.group(1))):
-	if (he.match(parsed.group(2))):
-		print('increased safehouse health')
-		print('decrement points')
-		print('print safehouse stats')
-	elif (at.match(parsed.group(2))):
-		print('increased safehouse attack')
-		print('decrement points')
-		print('print safehouse stats')
-	elif (de.match(parsed.group(2))):
-		print('increased safehouse defence')
-		print('decrement points')
-		print('print safehouse stats')
-	else:
-		print('re-loop safehouse')
-		# print error message and re-loop
-else:
-	print('re-loop epic fail')
-	# print error message and re-loop
+	objVar = safehouse
 
+if (he.match(parsed.group(2))):
+	attrVar = objVar.health
+elif (at.match(parsed.group(2))):
+	attrVar = objVar.attack
+elif (de.match(parsed.group(2))):
+	attrVar = objVar.defence
+elif (sp.match(parsed.group(2))):
+	attrVar = objVar.speed
 
-
+print(attrVar)
